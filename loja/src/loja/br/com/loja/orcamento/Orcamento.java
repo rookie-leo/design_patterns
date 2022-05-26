2baaec61fadfd4dcb0c2075abdb0e947ad88b18f
rookie-leo <proleonarpa@gmail.com>
2022-05-26 12:55:42 -0300

    Adicionando desconto extra

--- loja/src/loja/br/com/loja/orcamento/Orcamento.java
+++ loja/src/loja/br/com/loja/orcamento/Orcamento.java
@@ -6,6 +6,7 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private int qtdeItens;
+	private String situacao;
 	
 	public Orcamento(BigDecimal valor, int qtdeItens) {
 		this.valor = valor;
@@ -18,6 +19,22 @@ public class Orcamento {
 	
 	public int getQtdeItens() {
 		return qtdeItens;
+	}
+	
+	public String getSituacao() {
+		return situacao;
+	}
+	
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+		
+		if (situacao.equals("EM_ANALISE")) {
+			valorDoDescontoExtra = new BigDecimal("0.05");
+		} else if (situacao.equals("APROVADO")) {
+			valorDoDescontoExtra = new BigDecimal("0.02");
+		}
+		
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
 	}
 	
 }
