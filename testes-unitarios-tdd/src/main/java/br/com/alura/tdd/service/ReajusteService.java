ca9d2340daca6614fe9fb05ad37e01e899a8b181
rookie-leo <proleonarpa@gmail.com>
2022-06-08 18:48:11 -0300

    Refatorando com o pattern STRATEGY

--- testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,20 +8,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void reajustar(Funcionario funcionario, Desempenho desempenho) {
-		BigDecimal reajuste;
-		if (desempenho == Desempenho.A_DESEJAR) {
-			reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajustarSalario(reajuste);
-		} else if (desempenho == Desempenho.BOM) {
-			reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajustarSalario(reajuste);
-		} else if (desempenho == Desempenho.OTIMO) {
-			reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-			funcionario.reajustarSalario(reajuste);
-		} else {
-			throw new IllegalArgumentException("Desempenho não encontrado!");
-		}
-		
+		BigDecimal percentual = desempenho.percentualReajuste();
+		BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+		funcionario.reajustarSalario(reajuste);
 	}
 
 }
