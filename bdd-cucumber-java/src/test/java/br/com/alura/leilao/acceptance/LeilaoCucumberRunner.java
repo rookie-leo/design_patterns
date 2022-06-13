67f43e8655d81025ef923c3d75efdf9f9509d540
rookie-leo <proleonarpa@gmail.com>
2022-06-13 17:15:54 -0300

    Tags e LeilaoSteps

--- bdd-cucumber-java/src/test/java/br/com/alura/leilao/acceptance/LeilaoCucumberRunner.java
+++ bdd-cucumber-java/src/test/java/br/com/alura/leilao/acceptance/LeilaoCucumberRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features")// Integração entre Cucumber e JUnit
+@CucumberOptions(features = "classpath:features", // Integração entre Cucumber e JUnit 
+tags = "@leilao")//o campo tags indica ao cucumber qual funcionalidade deve ser testada no momento
 public class LeilaoCucumberRunner {
 
 }
---
+++ bdd-cucumber-java/src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,29 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LeilaoSteps {
+
+	@Dado("o usuario logado")
+	public void o_usuario_logado() {
+	}
+
+	@Quando("acessa a pagina de novo leilao")
+	public void acessa_a_pagina_de_novo_leilao() {
+	}
+	
+	@Quando("preenche o formulario com dados validos")
+	public void preenche_o_formulario_com_dados_validos() {
+	}
+	
+	@Entao("volta para a pagina de leiloes")
+	public void volta_para_a_pagina_de_leiloes() {
+	}
+	
+	@Entao("o novo leilao aparece na tabela")
+	public void o_novo_leilao_aparece_na_tabela() {
+	}
+	
+}
