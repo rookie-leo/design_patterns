aaf72e63bf1c52903d987c4b7d988442d2941e1e
rookie-leo <proleonarpa@gmail.com>
2022-06-13 17:54:12 -0300

    Implementado LeilaoSteps e corrigido bugs encontrados

--- bdd-cucumber-java/src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
+++ bdd-cucumber-java/src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -1,29 +1,47 @@
 package br.com.alura.leilao.acceptance.steps;
 
+import static org.junit.Assert.assertTrue;
+
+import br.com.alura.leilao.e2e.pages.Browser;
+import br.com.alura.leilao.e2e.pages.LeiloesPage;
+import br.com.alura.leilao.e2e.pages.LoginPage;
+import br.com.alura.leilao.e2e.pages.NovoLeilaoPage;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 
 public class LeilaoSteps {
 
+	private LoginPage loginPage;
+	private LeiloesPage leiloesPage;
+	private NovoLeilaoPage novoLeilaoPage;
+
 	@Dado("o usuario logado")
 	public void o_usuario_logado() {
+		Browser browser = new Browser();
+		browser.seed();
+		loginPage = browser.getLoginPage();
+		leiloesPage = loginPage.realizaLoginComoFulano();
 	}
 
 	@Quando("acessa a pagina de novo leilao")
 	public void acessa_a_pagina_de_novo_leilao() {
+		novoLeilaoPage = this.leiloesPage.visitaPaginaParaCriarUmNovoLeilao();
 	}
 	
 	@Quando("preenche o formulario com dados validos")
 	public void preenche_o_formulario_com_dados_validos() {
+		this.leiloesPage = this.novoLeilaoPage.preencheForm("Smart thermometer", "385", "13/06/2022");
 	}
 	
 	@Entao("volta para a pagina de leiloes")
 	public void volta_para_a_pagina_de_leiloes() {
+		assertTrue(leiloesPage.estaNaPaginaDeLeiloes());
 	}
 	
 	@Entao("o novo leilao aparece na tabela")
 	public void o_novo_leilao_aparece_na_tabela() {
+		assertTrue(this.leiloesPage.existe("Smart thermometer", "385", "13/06/2022", "fulano"));
 	}
 	
 }
--- bdd-cucumber-java/src/test/java/br/com/alura/leilao/e2e/pages/LeiloesPage.java
+++ bdd-cucumber-java/src/test/java/br/com/alura/leilao/e2e/pages/LeiloesPage.java
@@ -21,9 +21,12 @@ public class LeiloesPage {
 		driver.get(PAGE_URL);
 	}
 
-	public boolean existe(String nomeProduto, String valor, String usuario) {
-		return driver.getCurrentUrl().endsWith("/leiloes") && driver.getPageSource().contains(nomeProduto) && 
-				driver.getPageSource().contains(valor);
+	public boolean existe(String nomeProduto, String valor, String data, String nomeUsuario) {
+		return driver.getCurrentUrl().endsWith("/leiloes") 
+				&& driver.getPageSource().contains(nomeProduto) 
+				&& driver.getPageSource().contains(valor)
+				&& driver.getPageSource().contains(data)
+				&& driver.getPageSource().contains(nomeUsuario);
 	}
 	
 	public boolean estaNaPaginaDeLeiloes() {
